66a2fb14b08bf42de2e2283c594fc7b800ea24dc
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-02-09 17:26:18 +0300

    Create Shuffle_string.py

---
+++ Shuffle_string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        ans = []
+        freq = {}
+        for i in range(len(indices)):
+            freq [indices[i]] = s[i]
+        nums = sorted(freq.items(), key=lambda x:x[0])
+        for key, val in nums:
+            ans.append(val)
+
+        
+        return "".join(ans)
