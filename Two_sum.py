3b2ecfe08dc93422374bf46906249d21d1b4e6bb
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-02-09 16:51:18 +0300

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans = {}
+        for i,num in enumerate(nums):
+            diff = target - num
+            if diff in ans:
+                return [ans[diff], i]
+            ans[num] = i
