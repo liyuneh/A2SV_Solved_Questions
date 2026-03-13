f8872d88983ede16c9acd9f69564088a5022c3c2
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-03-13 16:36:08 +0300

    Create Minimum_moves_to_reach_target_score.py

---
+++ Minimum_moves_to_reach_target_score.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        count = 0
+        while maxDoubles > 0 and target > 1:
+            if target % 2 != 0 :
+                target -= 1
+            else:
+                target = target // 2
+                maxDoubles -= 1
+            count += 1
+        if target:
+            count += (target - 1)
+        return count 
