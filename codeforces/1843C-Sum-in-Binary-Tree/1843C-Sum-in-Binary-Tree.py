a223286021ec5fed015828714cbb3f1fb75fbf50
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-04-25 14:33:29 +0300

    Add solution for Sum in Binary Tree

---
+++ codeforces/1843C-Sum-in-Binary-Tree/1843C-Sum-in-Binary-Tree.py
@@ -0,0 +1,28 @@
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+from heapq import heappush, heappop, heapify
+from random import randint
+Random = randint(100, 10**4)
+Xor = lambda x:x ^ Random
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+
+def solve():
+    n = number()
+    total = 0
+    while n >= 1:
+        total += n
+        n //= 2
+    print(total)
+    return
+
+
+for _ in range(test_cases()):
+    solve()
