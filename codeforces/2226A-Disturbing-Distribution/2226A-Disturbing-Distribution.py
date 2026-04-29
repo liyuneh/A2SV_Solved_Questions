50d0ce68d13e7a6d9572bcb5ddc3e2fb305595d0
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-04-29 22:50:23 +0300

    Add solution for Disturbing Distribution

---
+++ codeforces/2226A-Disturbing-Distribution/2226A-Disturbing-Distribution.py
@@ -0,0 +1,30 @@
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+from heapq import heappush, heappop, heapify
+from random import randint
+Random = randint(100, 10**4)
+Xor = lambda x:x ^ Random
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+
+def solve():
+    n = number()
+    arr = numbers()
+    num = 0
+    for i in range(n):
+        if arr[i] != 1:
+            num += arr[i]
+
+    print(num if arr[-1] != 1 else num + 1)
+    return
+
+
+for _ in range(test_cases()):
+    solve()
