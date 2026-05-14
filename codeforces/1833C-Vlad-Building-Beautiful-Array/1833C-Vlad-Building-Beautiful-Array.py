ef1b3359352b4ea1d794b17d52088fc242445673
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-05-14 10:06:19 +0300

    Add solution for Vlad Building Beautiful Array

---
+++ codeforces/1833C-Vlad-Building-Beautiful-Array/1833C-Vlad-Building-Beautiful-Array.py
@@ -0,0 +1,32 @@
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+from heapq import heappush, heappop, heapify
+from random import randint
+Random = randint(100, 10**4)
+Xor = lambda x:x ^ Random
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+
+def solve():
+    n = number()
+    arr = numbers()
+    mn = min(arr)
+    if all(num % 2 == 0 for num in arr) or all(num % 2 == 1 for num in arr):
+        print("YES")
+        return 
+    if mn % 2 == 0:
+        print("NO")
+    else:
+        print("YES")
+    return
+
+
+for _ in range(test_cases()):
+    solve()
