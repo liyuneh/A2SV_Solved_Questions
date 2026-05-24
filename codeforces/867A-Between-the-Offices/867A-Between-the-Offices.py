ba32f1f2ba726b6163c7c63e7a02e23e46aac216
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-05-24 11:45:18 +0300

    Add solution for Between the Offices

---
+++ codeforces/867A-Between-the-Offices/867A-Between-the-Offices.py
@@ -0,0 +1,34 @@
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+from heapq import heappush, heappop, heapify
+from random import randint
+Random = randint(100, 10**4)
+Xor = lambda x:x ^ Random
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+
+def solve():
+    n = number()
+    s = list(word())
+
+    s = [1 if s[i] == "S" else 0 for i in range(len(s))]
+    
+    counts, countf = 0, 0
+    for i in range(1, len(s)):
+        if s[i] == 1 and s[i- 1] == 0:
+            countf += 1
+        elif s[i] == 0 and s[i-1] == 1:
+            counts += 1
+    print(yes_no(counts > countf))
+    return
+
+
+for _ in range(test_cases(1)):
+    solve()
