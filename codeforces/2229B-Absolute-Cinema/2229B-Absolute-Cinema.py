9f1dd10e537e621e5aeaf048b0a8fb8228a428d1
Liyuneh Rstey <149043085+liyuneh@users.noreply.github.com>
2026-06-05 22:58:01 +0300

    Add solution for Absolute Cinema

---
+++ codeforces/2229B-Absolute-Cinema/2229B-Absolute-Cinema.py
@@ -0,0 +1,34 @@
+import sys, math, heapq as heap, itertools
+from collections import defaultdict, Counter, deque
+from bisect import bisect_right, bisect_left
+from random import randint
+from heapq import heappush, heappop, heapify
+from random import randint
+Random = randint(100, 10**4)
+Xor = lambda x:x ^ Random
+
+number = lambda: int(sys.stdin.readline().strip())
+numbers = lambda: list(map(int, sys.stdin.readline().strip().split()))
+words = lambda: sys.stdin.readline().strip().split()
+word = lambda: sys.stdin.readline().strip()
+yes_no = lambda condition: 'YES' if condition else 'NO'
+test_cases = lambda inp=0: number() if not inp else inp
+
+def solve():
+    n = number()
+    a = numbers()
+    b = numbers()
+
+    for i in range(n):
+        if a[i] > b[i]:
+            b[i], a[i] = a[i], b[i]
+    print(max(a) + sum(b))
+
+
+
+
+    return
+
+
+for _ in range(test_cases()):
+    solve()
